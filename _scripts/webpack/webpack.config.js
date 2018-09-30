99ea0591a717d49e67b54800d2b5d684e18fcd54
Glenn de Haan <glenn@dehaan.cloud>
2018-09-30 18:05:55 +0200

    Modified the webpack config for a clean production build

--- _scripts/webpack/webpack.config.js
+++ _scripts/webpack/webpack.config.js
@@ -1,18 +1,23 @@
 const path = require('path');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const projectRoot = path.join(__dirname, '../..');
 const buildDirectory = path.join(projectRoot, 'frontend');
 const distDirectory = path.join(projectRoot, 'public/dist');
 
-module.exports = {
+const prod = process.env.NODE_ENV === "production";
+
+const config = {
     entry: {
         main: [
             path.join(buildDirectory, 'main.js'),
             path.join(buildDirectory, 'scss/style.scss')
         ]
     },
+    devtool: !prod ? 'inline-source-map' : '',
     output: {
         path: distDirectory,
         filename: '[name].[hash:6].js'
@@ -49,8 +54,8 @@ module.exports = {
                 test: /\.scss$/,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    'css-loader?url=false',
-                    'sass-loader'
+                    {loader: 'css-loader', options: {minimize: prod, url: false, sourceMap: !prod}},
+                    {loader: 'sass-loader', options: {sourceMap: !prod}}
                 ]
             }
         ]
@@ -64,3 +69,46 @@ module.exports = {
         })
     ]
 };
+
+if(prod) {
+    config.plugins.push(
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                mangle: true,
+                compress: {
+                    negate_iife: false,
+                    unsafe_comps: true,
+                    properties: true,
+                    keep_fargs: false,
+                    pure_getters: true,
+                    collapse_vars: true,
+                    unsafe: true,
+                    warnings: false,
+                    sequences: true,
+                    dead_code: true,
+                    drop_debugger: true,
+                    comparisons: true,
+                    conditionals: true,
+                    evaluate: true,
+                    booleans: true,
+                    loops: true,
+                    unused: true,
+                    hoist_funs: true,
+                    if_return: true,
+                    join_vars: true,
+                    drop_console: true,
+                    pure_funcs: ['classCallCheck', 'invariant', 'warning']
+                }
+            }
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {
+                discardComments: {removeAll: true}
+            }
+        })
+    );
+}
+
+module.exports = config;
