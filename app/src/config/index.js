e4da62e0da957e70ac27b2d5b90793a71b10720c
Glenn de Haan <glenn@dehaan.cloud>
2018-10-23 20:09:28 +0200

    Config can now autosave initial version

--- app/src/config/index.js
+++ app/src/config/index.js
@@ -1,2 +1,21 @@
+/**
+ * Import base packages
+ */
+const fs = require('fs');
+
+/**
+ * Check if we are using the dev version
+ */
 const dev = process.env.NODE_ENV !== 'production';
-module.exports = eval('require')(dev ? __dirname + '/config.json' : process.cwd() + '/config.json');
+
+/**
+ * Export the main config
+ */
+try {
+    module.exports = eval('require')(dev ? __dirname + '/config.json' : process.cwd() + '/config.json');
+} catch (e) {
+    const config = fs.readFileSync(__dirname + '/../../../_scripts/config/config.example.json', 'utf8');
+    fs.writeFileSync(dev ? __dirname + '/config.json' : process.cwd() + '/config.json', config);
+
+    module.exports = JSON.parse(config);
+}
