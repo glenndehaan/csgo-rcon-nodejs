09e882705e30abcd515af770303bb37af8e059c1
Glenn de Haan <glenn@dehaan.cloud>
2018-11-18 17:52:12 +0100

    Added a welcome message for the production build

--- app/src/server.js
+++ app/src/server.js
@@ -1,4 +1,9 @@
 /**
+ * Import external modules
+ */
+const fs = require("fs");
+
+/**
  * Import own modules
  */
 const version = require("./config/version");
@@ -27,11 +32,39 @@ if(!dev) {
     csgoConfig.init();
 }
 database.init();
-queue.init();
-web.init();
-if(config.integrations.challonge.enabled) {
-    challonge.init();
-}
 
 log.info("[SYSTEM] App running");
 log.info(`[SYSTEM] Version: ${version}`);
+log.info(`[SYSTEM] Support and Help: https://github.com/glenndehaan/csgo-rcon-nodejs`);
+
+/**
+ * Check if this is the first time running the app
+ */
+if(!dev) {
+    if (!fs.existsSync(`${process.cwd()}/LICENCE`) || !fs.existsSync(`${process.cwd()}/README.md`)) {
+        fs.writeFileSync(process.cwd() + '/LICENCE', fs.readFileSync(__dirname + '/../../LICENCE', 'utf8'));
+        fs.writeFileSync(process.cwd() + '/README.md', fs.readFileSync(__dirname + '/../../README.md', 'utf8'));
+
+        log.info("------------------------------------------");
+        log.info("Hi and thank you for using this piece of software!");
+        log.info("Go ahead and update the config.json to your needed then relaunch the software!");
+        log.info("The software will close in 5 seconds!");
+        log.info("------------------------------------------");
+
+        setTimeout(() => {
+            process.exit(0);
+        }, 5000)
+    } else {
+        queue.init();
+        web.init();
+        if(config.integrations.challonge.enabled) {
+            challonge.init();
+        }
+    }
+} else {
+    queue.init();
+    web.init();
+    if(config.integrations.challonge.enabled) {
+        challonge.init();
+    }
+}
