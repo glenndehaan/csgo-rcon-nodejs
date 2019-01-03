e785d582bc73f88da9ee0f449ae058436b275b5b
Glenn de Haan <glenn@dehaan.cloud>
2019-01-03 13:18:57 +0100

    Lang toggle + filters styling fixes tnx to @Hornebom

--- frontend/components/partials/Footer.js
+++ frontend/components/partials/Footer.js
@@ -2,6 +2,8 @@ import {h, Component} from 'preact';
 import {Link} from 'preact-router/match';
 import {connect} from "unistore/preact";
 
+import Language from "../icons/Language";
+
 class Footer extends Component {
     /**
      * Constructor
@@ -10,16 +12,35 @@ class Footer extends Component {
         super();
 
         this.state = {
-            loadTime: 0.00
+            loadTime: 0.00,
+            langToggleOpen: false
         }
     }
 
+    /**
+     * Runs then the component mounts
+     */
     componentDidMount() {
         const loadTime = (Date.now() - window.loadTime) / 1000;
 
         this.setState({
             loadTime: loadTime.toFixed(2)
         });
+    }
+
+    /**
+     * Toggles the lang dropdown
+     */
+    toggleButton() {
+        if(this.state.langToggleOpen) {
+            this.setState({
+                langToggleOpen: false
+            });
+        } else {
+            this.setState({
+                langToggleOpen: true
+            });
+        }
     }
 
     /**
@@ -32,6 +53,17 @@ class Footer extends Component {
             <footer className="footer">
                 <div className="container">
                     <span className="text">{this.props.lang.general.footer.loadedIn}: {this.state.loadTime} {this.props.lang.general.footer.seconds} | <Link href="/about">{this.props.lang.general.footer.about}</Link></span>
+                    <div className={`btn-group dropup ${this.state.langToggleOpen ? 'show' : ''}`} onClick={() => this.toggleButton()}>
+                        <button type="button" className="btn btn-sm btn-secondary dropdown-toggle language-icon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <Language/>
+                        </button>
+                        <div className={`dropdown-menu ${this.state.langToggleOpen ? 'show' : ''}`}>
+                            <a className="dropdown-item" href="#" native>EN</a>
+                            <a className="dropdown-item" href="#" native>FR</a>
+                            <a className="dropdown-item" href="#" native>DE</a>
+                            <a className="dropdown-item" href="#" native>NL</a>
+                        </div>
+                    </div>
                 </div>
             </footer>
         );
